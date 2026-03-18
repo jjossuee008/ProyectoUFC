b14fb06275c42ef6fe15d004fb261bff868e8ecf
jjossuee008 <josuemattab@hotmail.com>
2026-03-18 09:12:30 +0100

    Finalizacion clase para trabajar con archivos

--- src/UFC/funciones/ImportacionArchivo.java
+++ src/UFC/funciones/ImportacionArchivo.java
@@ -1,13 +1,58 @@
 package UFC.funciones;
 
+import UFC.peleadores.ConjuntoPeleadores;
+import UFC.peleadores.Peleadores;
+import UFC.vistas.VistaGeneral;
+
+/**
+ * Carga los datos que se le indiquen y exportara estos datos si asi se desea
+ * @aurhor Josué Mateos (jjossuee008)
+ * @version 1.0 (18032026)
+ */
 public class ImportacionArchivo {
 	
+	/** Almacen que guarda a los peleadores */
+	private static ConjuntoPeleadores almacen = new ConjuntoPeleadores();
 	
-	public static void importar() {
-		
+	/** Sacara los datos en un archivo fuera del programa */
+	public void exportar() {
+	    try (java.io.PrintWriter pw = new java.io.PrintWriter("datos.txt")) {
+	        for (Peleadores p : almacen.devolverElementos()) {
+	            // Escribimos los datos básicos separados por ";"
+	            pw.println(p.devolverDatos() + ";" + p.devolverEstadisticas());
+	        }
+	        VistaGeneral.mostrarAviso("Archivo guardado con éxito.");
+	    } catch (java.io.FileNotFoundException e) {
+	        VistaGeneral.mostrarAviso("No se pudo crear el archivo.");
+	    }
 	}
 	
-	public static void exportar() {
-		
+	/** Añadira los datos de un archivo que indique el usuario */
+	public void importar() {
+	    java.io.File archivo = new java.io.File("datos.txt");
+	    if (!archivo.exists()) {
+	        VistaGeneral.mostrarAviso("No hay datos previos para cargar.");
+	        return;
+	    }
+
+	    try (java.util.Scanner lector = new java.util.Scanner(archivo)) {
+	        while (lector.hasNextLine()) {
+	            String linea = lector.nextLine();
+	            String[] trozos = linea.split(";"); // Separa el texto por cada ";"
+	            
+	            // Creamos el peleador con los trozos (Ojo: convierte los números)
+	            Peleadores p = new Peleadores(
+	                trozos[0], // Nombre
+	                trozos[1], // Ranking
+	                Integer.parseInt(trozos[2]), // Victorias
+	                0, 0 // Rellenamos el resto con 0 o lo que necesites
+	            );
+	            
+	            almacen.alta(p);
+	        }
+	        VistaGeneral.mostrarAviso("Datos cargados correctamente.");
+	    } catch (Exception e) {
+	        VistaGeneral.mostrarAviso("Error al leer el archivo.");
+	    }
 	}
 }
